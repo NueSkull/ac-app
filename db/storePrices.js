4a9751d7e42a85599d990395a594f78b8fb20e7e
NueSkull <markjtravers@hotmail.com>
2025-10-23 16:37:00 +0100

    intermittent change

--- db/storePrices.js
+++ db/storePrices.js
@@ -7,17 +7,15 @@ const jsonToArray = require('../utils/jsonToArray')
 const injectPricesTable = async (csvJson) => {
     await db.query(`DROP TABLE IF EXISTS prices;`);
     await db.query(`CREATE TABLE prices (
-        uniqueID VARCHAR PRIMARY KEY,
-        sku VARCHAR,
+        sku VARCHAR PRIMARY KEY,
         primary_sku VARCHAR,
-        price FLOAT(2),
-        customer VARCHAR
+        price FLOAT(2)
         );`);
     const csvArrayd = await jsonToArray(csvJson);
     const skudArray = csvArrayd.map((row) => {
-        return [(row[2]+row[0]), row[0], row[0].substring(0,9), row[1], row[2]]
+        return [row[0], row[0].substring(0,9), row[1]]
     })
-    await db.query(format(`INSERT INTO prices (uniqueID, sku, primary_sku, price, customer) VALUES %L ON CONFLICT (uniqueID) DO NOTHING;`, skudArray));
+    await db.query(format(`INSERT INTO prices (sku, primary_sku, price) VALUES %L ON CONFLICT (sku) DO NOTHING;`, skudArray));
 
     
 }
