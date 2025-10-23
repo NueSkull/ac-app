1787c69961736f13e6a6afda73eadff653b7c2a1
NueSkull <markjtravers@hotmail.com>
2025-10-23 16:56:50 +0100

    Modified getStyleInfo to get correct db table

--- models/getStyleInfo.js
+++ models/getStyleInfo.js
@@ -5,7 +5,7 @@ exports.getStyleInfo = async (sku) => {
             const query = `
                 SELECT a.size_value, a.stock_level, p.price 
                 FROM stock AS a
-                JOIN prices_apparel_catalogue AS p ON a.sku = p.sku
+                JOIN prices AS p ON a.sku = p.sku
                 WHERE a.primary_sku = $1
                 ORDER BY a.alpha_order ASC;
             `;
