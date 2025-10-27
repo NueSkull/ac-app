5b0ddbe77412b158ac828f24199248745986c289
NueSkull <markjtravers@hotmail.com>
2025-10-27 14:10:02 +0000

    Brand Markups added

--- controllers/getStyleInfo.js
+++ controllers/getStyleInfo.js
@@ -1,7 +1,7 @@
 const { getStyleInfo } = require('../models/getStyleInfo')
 
 exports.getStyleInfo = async (req, res, next) => {
-    const {sku, brand} = req.params;
-    const styleInfo = await getStyleInfo(sku, brand)
+    const {sku, brand, subdom} = req.params;
+    const styleInfo = await getStyleInfo(sku, brand, subdom)
     res.status(200).send({styleInfo});
 }
--- models/getStyleInfo.js
+++ models/getStyleInfo.js
@@ -1,7 +1,24 @@
 const db = require("../db/connection");
+const { getPricingF } = require('./pricinglogic');
 
-exports.getStyleInfo = async (sku, brand) => {
-    console.log(`Style info request received for ${sku}`);
+exports.getStyleInfo = async (sku, brand, subdom) => {
+
+    let returningPrices = '';
+
+    async function brandMarkup(brand, prices) {
+        const updatedPrices = prices.map((size) => {
+            for(const rule in brand) {
+                if(size.price >= brand[rule].low_break && size.price <= brand[rule].high_break) {
+                    return {...size, price: Math.ceil(((size.price / 100) * (brand[rule].percentage + 100)) * 100) / 100}
+                }
+            }
+
+            return size;
+        })
+        return updatedPrices
+    }
+    
+    console.log(`Style info request received for ${sku} - ${brand} - ${subdom}`);
             const query = `
                 SELECT a.size_value, a.stock_level, p.price 
                 FROM stock AS a
@@ -9,6 +26,19 @@ exports.getStyleInfo = async (sku, brand) => {
                 WHERE a.primary_sku = $1
                 ORDER BY a.alpha_order ASC;
             `;
+
             const response = await db.query(query, [sku]);
-            return response.rows;
+            returningPrices = response.rows;
+
+            if(subdom && brand) {
+                const priceMatrix = await getPricingF(subdom);
+                const filterForBrand = priceMatrix.filter((rule) => {
+                    return rule.brand === brand;
+                })
+                if(filterForBrand.length > 0) {
+                  returningPrices = await brandMarkup(filterForBrand, returningPrices);
+                }
+            }
+
+            return returningPrices;
 }
